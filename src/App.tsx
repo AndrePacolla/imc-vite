9443281e48ba21bb75819db96ad93d1aa72dc8bc
AndrePacolla <andreluizzpacolla@outlook.com>
2025-05-24 16:11:23 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.module.css';
+import poweredImage from "./assets/powered.png";
+
+function App() {
+ 
+
+  return (
+  
+    <div >
+      <header>
+        <div>
+          <img src={poweredImage} width={150}/>
+
+        </div>
+      </header>
+    </div>
+  
+  )
+}
+
+export default App;
