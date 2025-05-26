e6a47a8a99a9e3cdad3f6572fb3d3c4c56c42185
AndrePacolla <andreluizzpacolla@outlook.com>
2025-05-26 16:35:30 -0300

    montado area do header e ja deixando responsivo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.module.css';
+import styles from './App.module.css';
 import poweredImage from "./assets/powered.png";
 
 function App() {
@@ -6,13 +6,21 @@ function App() {
 
   return (
   
-    <div >
-      <header>
-        <div>
-          <img src={poweredImage} width={150}/>
+    <div className={styles.main} >
+      <header className={styles.header}>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} width={250}/>
 
         </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          testando 123
+        </div>
+        <div className={styles.rightSide}>
+          testado 123
+        </div>
+      </div>
     </div>
   
   )
