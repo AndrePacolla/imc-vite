dc8c54492368d6d1fc2c0bb4ccab6a1cf4c13486
AndrePacolla <andreluizzpacolla@outlook.com>
2025-06-27 17:04:13 -0300

    lógica em calculo imc

--- src/helpers/imc.ts
+++ src/helpers/imc.ts
@@ -3,6 +3,7 @@ export type Level = {
     color: string;
     icon: "down" | "up";
     imc: number[];
+    myImc?: number;
 }
 
 export const levels: Level[] = [
@@ -14,5 +15,14 @@ export const levels: Level[] = [
 
 
 export const calculateImc = (height: number, weight: number) => {
+    const imc = weight / (height * height);
+
+    for (let i in levels ){
+        if(imc >= levels[i].imc[0] && imc < levels[i].imc[1]){
+            levels[i].myImc = imc;
+            return levels[i];
+        }
+    }
+    return null;
 
 }
